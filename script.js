be3501c2c020582e6144b01f996dac7a2c7ec46e
Shiva-Prasad83 <shivaprasad.ch83@gmail.com>
2025-09-09 23:06:46 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 //your JS code here. If required.
+const button=document.getElementsByTagName("input")[0];
+button.addEventListener("click",(e)=>{
+	const element=document.querySelector("#colorSelect option:checked");
+	element.remove();
+})
+
